a4b4fcc2f420fac002e978cbd707155444995cbf
Jake Wnuk <jakewnuk@gmail.com>
2022-07-10 14:09:39 -0400

    manual test
    1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pi-hole Blocklist
+Aggregates Pi-Hole lists into a single list with automation. Please consider
+supporting the creators listed in `sources.json`.
--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+mkdir -p ./sources
 for line in $(cat sources.json | jq -r '.sources[] | [.name,.url] | @csv')
 do
   name="$(echo $line | cut -d "," -f 1 | tr -d '"')"
   url="$(echo $line | cut -d "," -f 2 | tr -d '"')"
   echo "Fetching $name" 
-  curl $url -o $name.txt
+  curl $url -o ./sources/$name.txt --retry 5 --max-time 30 --retry-delay 60 --retry-max-time 180
 done
 
-cat *.txt | grep -vE '#|127.0.0.1|localhost|::0|::1|::2|::3' | sed 's/\r$//;s/0.0.0.0 //g' | sort -u > hosts.lst
-rm *.txt
+cat *.txt | grep -vE '#|127.0.0.1|localhost|::0|::1|::2|::3' | sed 's/\r$//;s/0.0.0.0 //g' | sort -u > ./sources/hosts.lst
