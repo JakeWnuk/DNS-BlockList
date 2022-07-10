b3027d447143b20ec384a30a5eba34bf5c5c2453
Jake Wnuk <jakewnuk@gmail.com>
2022-07-10 14:24:23 -0400

    add regex validation
    added additional validation for input URLs

--- fetch.sh
+++ fetch.sh
@@ -9,4 +9,4 @@ do
   curl $url -o ./sources/$name.txt --retry 5 --max-time 30 --retry-delay 60 --retry-max-time 180
 done
 
-cat *.txt | grep -vE '#|127.0.0.1|localhost|::0|::1|::2|::3' | sed 's/\r$//;s/0.0.0.0 //g' | sort -u > ./sources/hosts.lst
+cat ./sources/*.txt | grep -vE '#|127.0.0.1|localhost|::0|::1|::2|::3' | sed 's/\r$//;s/0.0.0.0 //g' | grep -P "(?=^.{4,253}$)(^(?:[a-zA-Z0-9](?:(?:[a-zA-Z0-9\-]){0,61}[a-zA-Z0-9])?\.)+([a-zA-Z]{2,}|xn--[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])$)" | sort -u > hosts.lst
